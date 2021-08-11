6bfdf147c58cb0d942a8300f1368e024417683de
Ernest-2020 <73068574+Ernest-2020@users.noreply.github.com>
2021-08-11 22:39:50 +0300

    add Lesson 8

--- Assets/Scripts/MainMenuView.cs
+++ Assets/Scripts/MainMenuView.cs
@@ -6,15 +6,28 @@ public class MainMenuView : MonoBehaviour
 {
     [SerializeField] 
     private Button _buttonStart;
-        
-    public void Init(UnityAction startGame)
+
+    [SerializeField]
+    private Button _buttonReward;
+
+    [SerializeField]
+    private Button _buttonExit;
+
+    public void Init(UnityAction startGame, UnityAction dailyReward)
     {
         _buttonStart.onClick.AddListener(startGame);
+        _buttonReward.onClick.AddListener(dailyReward);
+        _buttonExit.onClick.AddListener(ExitGame);
     }
-
     protected void OnDestroy()
     {
         _buttonStart.onClick.RemoveAllListeners();
+        _buttonReward.onClick.RemoveAllListeners();
+        _buttonExit.onClick.RemoveAllListeners();
+    }
+    private void ExitGame()
+    {
+        Application.Quit();
     }
 }
 
